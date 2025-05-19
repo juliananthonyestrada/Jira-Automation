320e7baad18103579a656de7cd931f59596e3598
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2025-05-18 21:47:43 -0400

    Add files via upload
    Adding scripts that retrieve all users on my site

---
+++ get-all-users.py
@@ -0,0 +1,35 @@
+import requests  # For making HTTP requests
+from requests.auth import HTTPBasicAuth  # For basic API authentication
+import json  # For handling JSON data
+from dotenv import load_dotenv  # Loads environment variables from a .env file
+import os  # Provides access to environment variables and operating system features
+
+load_dotenv()  # Load environment variables from the .env file
+
+# Jira Cloud API endpoint for creating issues
+url = os.getenv("url_get_users")  
+
+# Jira account credentials
+api_token = os.getenv("api_token")  
+email = os.getenv("email")
+
+# Tell Jira we want the response data formatted as JSON
+headers = {
+  "Accept": "application/json"
+}
+
+def get_all_users():
+    # Sends an HTTP GET request to Jira to retrieve all users, with headers and authentication
+    response = requests.get(
+        url,
+        headers=headers,
+        auth=(email, api_token)
+    )
+
+    users = response.json()  # Parse the JSON response into a Python dictionary
+
+    for i, user in enumerate(users):
+        print(i, user["displayName"])
+
+if __name__ == "__main__":
+    get_all_users()
